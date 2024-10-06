797f1d40eeb6ac329356953ec1f05e40f48b7b15
1119-DuyNguyen <thanhduy191103@gmail.com>
2024-10-07 00:10:57 +0700

    setup nextauth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,35 @@
-import { handlers } from '@/auth';
-export const { GET, POST } = handlers;
+import NextAuth from "next-auth";
+import envConfig from "@/config";
+
+type ProfileSpring={
+    sub: string;
+    name: string;
+    email: string;
+    picture: string;
+}
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    {
+        id: "spring",
+        name: "Spring oauth",
+        type: "oauth",
+        authorization: envConfig.OAUTH_AUTH_URL+ "/oauth2/authorize",
+        token: envConfig.OAUTH_AUTH_URL+ "/oauth2/token",
+        userinfo: envConfig.OAUTH_AUTH_URL+ "/userinfo",
+        idToken: true,
+        checks: ["pkce", "state"],
+        profile(profile: ProfileSpring) {
+          return {
+            id: profile.sub,
+            name: profile.name,
+            email: profile.email,
+            image: profile.picture,
+          }
+        },
+      },
+    // ...add more providers here
+  ],
+}
+
+export default NextAuth(`authOptions`)
--- src/config.ts
+++ src/config.ts
@@ -4,12 +4,16 @@ const configSchema = z.object({
   NEXT_PUBLIC_API_ENDPOINT: z.string(),
   NEXT_PUBLIC_URL: z.string(),
   NEXT_PUBLIC_ACCESS_TOKEN: z.string(),
+  OAUTH_AUTH_URL: z.string().default("http://localhost:8080"),
+  OAUTH_AUTH_ID: z.string().default("public-client"),
 });
 
 const configProject = configSchema.safeParse({
   NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
   NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
   NEXT_PUBLIC_ACCESS_TOKEN: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
+  OAUTH_AUTH_URL: process.env.NEXT_PUBLIC_OAUTH_AUTH_URL,
+  OAUTH_AUTH_ID: process.env.NEXT_PUBLIC_OAUTH_AUTH_ID,
 });
 
 if (!configProject.success) {
