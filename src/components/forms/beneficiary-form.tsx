41ee6bf849e04e42640feac22f1e94fe0fa4c77f
Tiền Minh Vy <tienminhvydev@gmail.com>
2024-10-10 10:02:22 +0700

    feat: sending simple beneficiary now available

--- src/components/forms/beneficiary-form.tsx
+++ src/components/forms/beneficiary-form.tsx
@@ -1,4 +1,5 @@
-import {Form, FormControl, FormField, FormItem, FormLabel} from "@/components/ui/form"
+'use client';
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form"
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -8,12 +9,16 @@ import { z } from "zod"
 
 const formSchema = z.object({
     situationDetail: z.string().min(10),
-    supportReceived: z.number().min(10000)
+    supportReceived: z.string().refine((val) => !Number.isNaN(parseInt(val, 10)), {
+        message: "Expected number, received a string"
+      })
 })
 
 type BeneficiaryFormValues = z.infer<typeof formSchema>
 
 export default function BeneficiaryForm() {
+
+    const apiURL = process.env.NEXT_PUBLIC_API_ENDPOINT;
 
     const [loading, setLoading] = useState(false);
 
@@ -21,22 +26,52 @@ export default function BeneficiaryForm() {
         resolver: zodResolver(formSchema)
     })
 
+    const onSubmit = async (data: BeneficiaryFormValues) => {
+        console.log(true);
+        
+        fetch(`${apiURL}/beneficiaries`, {
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer " + process.env.NEXT_PUBLIC_ACCESS_TOKEN,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                situationDetail: data.situationDetail,
+                supportReceived: data.supportReceived
+            })
+        }).then(res => res.json())
+        .then(res => console.log(res))
+    }
+
     return <>
         <Form {...form}>
-            <FormField control={form.control} 
-            name="supportReceived"
-            render={({ field }) => (
-            <FormItem>
-                <FormLabel>Số tiền dự kiến</FormLabel>
-                <FormControl>
-                    <Input type="number" placeholder="Nhập số tiền dự kiến" disabled={loading} {...field} ></Input>
-                </FormControl>
-            </FormItem>
-            )} />
-
-            <Button disabled={loading} className="ml-auto w-full" type="submit">
-                Gửi
-            </Button>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+                <FormField control={form.control} 
+                name="situationDetail"
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Chi tiết tình trạng cần hỗ trợ</FormLabel>
+                        <FormControl>
+                            <Input type="text" placeholder="Chi tiết tình trạng cần hỗ trợ" disabled={loading} {...field}></Input>
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                )} />
+                <FormField control={form.control} 
+                name="supportReceived"
+                render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Số tiền dự kiến</FormLabel>
+                            <FormControl>
+                                <Input type="number" placeholder="Nhập số tiền dự kiến" disabled={loading} {...field}></Input>
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                )} />
+                <Button disabled={loading} className="ml-auto w-full" type="submit">
+                    Gửi
+                </Button>
+            </form>
         </Form>
     </>
 }
