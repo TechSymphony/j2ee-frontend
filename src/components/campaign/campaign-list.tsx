b89378e975f57d1f3f219a86955f85834bbff578
Edric <datnguyen2132003@gmail.com>
2024-10-19 03:10:37 +0700

    feat: show list campaign

--- src/components/campaign/campaign-list.tsx
+++ src/components/campaign/campaign-list.tsx
@@ -1,14 +1,16 @@
 import Campaign from "@/components/campaign/campaign";
 import React from "react";
+import { useGetCampaignListQuery } from "@/queries/useCampaign";
+
 
 export default function CampaignList() {
+  const { data: campaignListData } = useGetCampaignListQuery();
+  const campaigns = campaignListData?.payload ?? [];
   return (
     <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-      {Array(6)
-        .fill(0)
-        .map((_, index) => (
-          <Campaign key={index} />
-        ))}
+      {campaigns.map((campaign, index) => (
+        <Campaign key={index} data={campaign} />
+      ))}
     </div>
   );
 }
