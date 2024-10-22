45669e4b58be4263408addec34004cfbf947c166
Nguyen Thanh Duy <62139508+1119-DuyNguyen@users.noreply.github.com>
2024-10-22 22:44:54 +0700

    Tlna 39 display categories multi level in frontend menu with scope permissions (#12)
    * setup categories data
    
    * setup structure
    
    * responsive category menu and get permissions

--- src/components/category-menu.tsx
+++ src/components/category-menu.tsx
@@ -1,76 +1,97 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import Link from "next/link"
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu"
-import { CategoryMenu } from "@/schemas/category.schema"
-
-
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { CategoryMenu } from "@/schemas/category.schema";
 
 interface NavigationMenuProps {
-  categories?: CategoryMenu[] | undefined;
+    categories?: CategoryMenu[] | undefined;
 }
 
 // Responsive Navigation Menu Component that accepts categories as a prop
-export const NavigationMenuComponent: React.FC<NavigationMenuProps> = ({ categories }) => {
-  if(!categories) return <></>;
-  return (
-    <NavigationMenu>
-      <NavigationMenuList >
-        {categories.map((category) => (
-          <NavigationMenuItem key={category.id}>
-            <NavigationMenuTrigger>{category.name}</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              {category.children && category.children.length > 0 ? (
-                <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                  {category.children.map((child) => (
-                    <ListItem key={child.id} item={child} />
-                  ))}
-                </ul>
-              ) : (
-                <Link href={category.name}>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    {category.name}
-                  </NavigationMenuLink>
-                </Link>
-              )}
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        ))}
-      </NavigationMenuList>
-    </NavigationMenu>
-  )
-}
+export const CategoryMenuComponent: React.FC<NavigationMenuProps> = ({
+    categories,
+}) => {
+    if (!categories) return <></>;
+    return (
+        <NavigationMenu>
+            <NavigationMenuList className="
+            flex-col justify-start gap-6 text-lg font-medium items-start 
+            md:flex md:flex-row md:items-center md:grow md:justify-center md:gap-5 md:text-sm lg:gap-6">
+                {categories.map((category) => (
+                    <NavigationMenuItem key={category.id} className="relative">
+                            {category.children &&
+                            category.children.length > 0 ? (
+                                <>
+                                <NavigationMenuTrigger className="space-y-1 rounded-md leading-none no-underline outline-none transition-colors">
+                                    <NavigationMenuLink
+                                        className={navigationMenuTriggerStyle()}
+                                    >
+                                        {category.name}
+                                    </NavigationMenuLink>
+                                </NavigationMenuTrigger>
+                                <NavigationMenuContent>
+                                <ul className="min-w-4 md:whitespace-nowrap">
+                                    {category.children.map((child) => (
+                                        <ListItem key={child.id} item={child} />
+                                    ))}
+                                </ul>
+
+                                </NavigationMenuContent>
+                                </>
+                            ) : (
+                                <Link
+                                href={""}
+                                key={category.id}
+                                className="text-muted-foreground"
+                            >
+                                {category.name}
+                            </Link>
+                            )}
+                    </NavigationMenuItem>
+                ))}
+            </NavigationMenuList>
+        </NavigationMenu>
+    );
+};
 
 // ListItem Component to handle subcategories recursively
 const ListItem: React.FC<{ item: CategoryMenu }> = ({ item }) => (
-  <li >
-    <NavigationMenuLink asChild>
-      <Link href={item.name}>
-        <div
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
-          )}
-        >
-          <div className="text-sm font-medium leading-none">{item.name}</div>
-          {item.children && item.children.length > 0 && (
-            <ul className="ml-4">
-              {item.children.map((subitem) => (
-                <ListItem key={subitem.id} item={subitem} />
-              ))}
-            </ul>
-          )}
-        </div>
-      </Link>
-    </NavigationMenuLink>
-  </li>
-)
+    <li className="relative">
+        <NavigationMenuLink asChild>
+            <Link href={item.name}>
+                <li
+                    className={cn(
+                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-pink-50  focus:bg-pink-50"
+                    )}
+                >
+                    {/* <div className="text-sm font-medium leading-none">
+                    {item.name}
+                    </div> */}
+                    <NavigationMenuLink asChild>
+                            <Link href="/vi-nhan-ai/tu-thien/giup-do-tre-em?fromType=nav_menu" className="text-muted-foreground block p-2 ">
+                            {item.name}
+                            </Link>
+                          </NavigationMenuLink>
+                    {/* {item.children && item.children.length > 0 && (
+                        <ul className="ml-4">
+                            {item.children.map((subitem) => (
+                                <ListItem key={subitem.id} item={subitem} />
+                            ))}
+                        </ul>
+                    )} */}
+                </li>
+            </Link>
+        </NavigationMenuLink>
+    </li>
+);
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -8,76 +8,80 @@ import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import NavItems from "@/app/(public)/nav-items";
 import Image from "next/image";
-import { NavigationMenuComponent } from '@/components/category-menu';
+import { CategoryMenuComponent } from "@/components/category-menu";
 import { useGetCategoryMenus } from "@/queries/useCategory";
+import DarkModeToggle from "../dark-mode-toggle";
 
 export default function Header() {
-  // const { data } = useGetCategoryMenus();
-  // const categorieMenuList = data?.payload;
+    const { data } = useGetCategoryMenus();
+    const categorieMenuList = data?.payload;
 
-  return (
-    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 z-20">
+    return (
+        <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 z-20">
+            <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:grow md:justify-center md:gap-5 md:text-sm lg:gap-6">
+                <Link
+                    href="#"
+                    className="flex items-center gap-2 text-lg font-semibold md:text-base"
+                >
+                    {/* <Package2 className="h-6 w-6" /> */}
+                    <Image
+                        src="/static/images/logo.png"
+                        alt="Logo"
+                        width={36}
+                        height={36}
+                        quality={100}
+                        className="h-6 w-6"
+                    />
 
-    <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:grow md:justify-center md:gap-5 md:text-sm lg:gap-6">
-      <Link
-        href="#"
-        className="flex items-center gap-2 text-lg font-semibold md:text-base"
-      >
-        {/* <Package2 className="h-6 w-6" /> */}
-        <Image
-          src="/static/images/logo.png"
-          alt="Logo"
-          width={36}
-          height={36}
-          quality={100}
-          className="h-6 w-6"
-        />
+                    <span className="sr-only">Acme Inc</span>
+                </Link>
+                <NavItems className="transition-colors hover:text-foreground flex-shrink-0" />
+                <CategoryMenuComponent
+                    categories={categorieMenuList}
+                ></CategoryMenuComponent>
+            </nav>
+            <Sheet>
+                <SheetTrigger asChild>
+                    <Button
+                        variant="outline"
+                        size="icon"
+                        className="shrink-0 md:hidden"
+                    >
+                        <Menu className="h-5 w-5" />
+                        <span className="sr-only">Toggle navigation menu</span>
+                    </Button>
+                </SheetTrigger>
+                <SheetContent side="left">
+                    <nav className="grid gap-6 text-lg font-medium">
+                        <Link
+                            href="#"
+                            className="flex items-center gap-2 text-lg font-semibold"
+                        >
+                            <Package2 className="h-6 w-6" />
+                            <span className="sr-only">Acme Inc</span>
+                        </Link>
+                        <NavItems className="text-muted-foreground transition-colors hover:text-foreground" />
+                        <CategoryMenuComponent
+                            categories={categorieMenuList}
+                        ></CategoryMenuComponent>
+                    </nav>
+                </SheetContent>
+                <div className="ml-auto">
+                    <DarkModeToggle />
+                </div>
+            </Sheet>
 
-        <span className="sr-only">Acme Inc</span>
-      </Link>
-      <NavItems className="transition-colors hover:text-foreground flex-shrink-0" />
-      {/* <NavigationMenuComponent categories={categorieMenuList}></NavigationMenuComponent> */}
-
-    </nav>
-    <Sheet>
-      <SheetTrigger asChild>
-        <Button
-          variant="outline"
-          size="icon"
-          className="shrink-0 md:hidden"
-        >
-          <Menu className="h-5 w-5" />
-          <span className="sr-only">Toggle navigation menu</span>
-        </Button>
-      </SheetTrigger>
-      <SheetContent side="left">
-        <nav className="grid gap-6 text-lg font-medium">
-          <Link
-            href="#"
-            className="flex items-center gap-2 text-lg font-semibold"
-          >
-            <Package2 className="h-6 w-6" />
-            <span className="sr-only">Acme Inc</span>
-          </Link>
-
-          <NavItems className="text-muted-foreground transition-colors hover:text-foreground" />
-        </nav>
-      </SheetContent>
-    </Sheet>
-    {/* <div className="ml-auto">
-      <DarkModeToggle />
-    </div> */}
-  </header>
-    // <header className="sticky inset-x-0 top-0 w-full">
-    //   <nav className="flex items-center justify-between px-4 py-2 md:justify-end">
-    //     <div className={cn('block lg:!hidden')}>
-    //       <MobileSidebar />
-    //     </div>
-    //     <div className="flex items-center gap-2">
-    //       <UserNav />
-    //       <ThemeToggle />
-    //     </div>
-    //   </nav>
-    // </header>
-  );
+        </header>
+        // <header className="sticky inset-x-0 top-0 w-full">
+        //   <nav className="flex items-center justify-between px-4 py-2 md:justify-end">
+        //     <div className={cn('block lg:!hidden')}>
+        //       <MobileSidebar />
+        //     </div>
+        //     <div className="flex items-center gap-2">
+        //       <UserNav />
+        //       <ThemeToggle />
+        //     </div>
+        //   </nav>
+        // </header>
+    );
 }
