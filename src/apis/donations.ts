d2ed114a38768e5c23ea971cd5e02a51839fb08d
imstephen <114798882+HoangVuong1910@users.noreply.github.com>
2024-11-05 01:33:57 +0700

    Tlna 82 show my donation (#19)
    * feat: donation
    - layout for personal user
    - my donation, all donations

--- src/apis/donations.ts
+++ src/apis/donations.ts
@@ -1,11 +1,10 @@
 import http from "@/lib/http";
-import {
-    DonationListResType,
-} from "@/schemas/donation.schema";
+import { DonationListResType } from "@/schemas/donation.schema";
 
 const prefix = "/donations";
 
 const donationApi = {
-    getDonationList: () => http.get<DonationListResType>(prefix),
+  getDonationList: () => http.get<DonationListResType>(prefix),
+  getMyDonationList: () => http.get<DonationListResType>(`/me${prefix}`),
 };
 export default donationApi;
--- src/queries/useDonation.tsx
+++ src/queries/useDonation.tsx
@@ -1,14 +1,21 @@
 import donationApi from "@/apis/donations";
+import { QueryConfig } from "@/hooks/useQueryConfig";
 // import { UpdateCampaignBodyType } from "@/schemas/campaign.schema";
 import { useQuery } from "@tanstack/react-query";
 
-export const useGetDonationListQuery = () => {
-    return useQuery({
-        queryKey: ["donations"],
-        queryFn: donationApi.getDonationList,
-        staleTime: 0,
-        enabled: true,
-    });
+export const useGetDonationListQuery = (queryConfig?: QueryConfig) => {
+  return useQuery({
+    queryKey: ["donations", queryConfig],
+    queryFn: donationApi.getDonationList,
+    staleTime: 0,
+    enabled: true,
+  });
 };
-
-
+export const useGetMyDonationListQuery = (queryConfig?: QueryConfig) => {
+  return useQuery({
+    queryKey: ["my-donations", queryConfig],
+    queryFn: donationApi.getMyDonationList,
+    staleTime: 0,
+    enabled: true,
+  });
+};
